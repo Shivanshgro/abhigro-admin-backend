f314b99fe54535a1228aee4bdd5521a4b9875992
ABHISEHKBACHANNA <Abhishek.bachanna@revappayyaitservices.com>
2026-06-23 08:24:11 +0530

    fix: restore adminController + wire partner routes

--- src/controllers/partnerAdminController.js
+++ src/controllers/partnerAdminController.js
@@ -1,4 +1,4 @@
-﻿const pool = require("../config/db");
+const pool = require("../config/db");
 
 exports.getVendors = async (req, res) => {
   try {
--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`SERVER STARTED ON PORT ${PORT}`);
+  console.log(`SERVER STARTED ON PORT ${PORT} - build v2 admin-fix`);
 });
